036ce90d0add8c942c9790a28c69ef58ebe1da22
Briana McCloud <bmccloud26122@gmail.com>
2022-07-31 18:53:49 -0400

    added status lines

---
+++ .history/src/components/App_20220731155853.js
@@ -0,0 +1,15 @@
+import "../styles/App.scss";
+
+function App() {
+  
+  return (
+    <div className="App">
+      <h1>Task Management</h1>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/src/components/App_20220731155907.js
@@ -0,0 +1,14 @@
+import "../styles/App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task Management</h1>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
